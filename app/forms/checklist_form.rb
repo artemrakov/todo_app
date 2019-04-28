f4b0e332b54296f91954ab4a6aba514b1ab5c1e1
Artem Rakov <scoff1k@gmail.com>
2019-04-29 02:59:36 +0300

    improved checklist form

--- app/forms/checklist_form.rb
+++ app/forms/checklist_form.rb
@@ -8,17 +8,21 @@ class ChecklistForm
     :checklist
   )
 
+  def initialize(attributes)
+    super(attributes)
+    @checklist = Checklist.new(title: title, user: user, checklist_template: checklist_template)
+  end
+
   def save
     return false unless valid?
 
-    @checklist = Checklist.new(title: title, user: user, checklist_template: checklist_template)
     create_items
-    @checklist.save
+    checklist.save
   end
 
   def create_items
     checklist_template.template_items.each do |template_item|
-      @checklist.items.build(template_item: template_item)
+      checklist.items.build(template_item: template_item)
     end
   end
 end
