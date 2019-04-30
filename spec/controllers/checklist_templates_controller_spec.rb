e3a419757eba40c6f534f7d5933f8fce153c3690
Artem Rakov <scoff1k@gmail.com>
2019-04-30 17:46:48 +0300

    remove controller tests

--- spec/controllers/checklist_templates_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ChecklistTemplatesController, type: :controller do
-
-end
--- spec/controllers/template_item_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe TemplateItemsController, type: :controller do
-
-end
