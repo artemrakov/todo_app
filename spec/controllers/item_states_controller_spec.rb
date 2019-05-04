c44d98b5d039770b69e89a83b9d3f4f57ea10bbf
Artem Rakov <scoff1k@gmail.com>
2019-05-04 16:18:09 +0300

    added tests to item states

---
+++ spec/controllers/item_states_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe ItemStatesController, type: :controller do
+  let(:user) { create(:user) }
+  let(:item) { create(:item) }
+  let(:item_done) { create(:item, :complete) }
+
+  describe '#create' do
+    context 'as an authenticated user' do
+      it 'changes status of the item to done' do
+        sign_in user
+        post :create, params: { id: item.id }
+        expect(item.reload.state).to eq 'done'
+      end
+    end
+  end
+
+  describe '#destroy' do
+    context 'as an authenticated user' do
+      it 'changes status of item to not_done' do
+        sign_in user
+        delete :destroy, params: { id: item_done.id }
+        expect(item_done.reload.state).to eq 'not_done'
+      end
+    end
+  end
+end
