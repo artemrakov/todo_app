88ccc7670862988e5caf4c1990675acf60128b4c
Artem Rakov <scoff1k@gmail.com>
2019-05-17 20:35:26 +0300

    added reek

--- spec/controllers/checklist_templates_controller_spec.rb
+++ spec/controllers/checklist_templates_controller_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe ChecklistTemplatesController, type: :controller do
 
       it 'searches by the provided keyword' do
         sign_in user
-        expect(checklist_template_class).to receive_message_chain(:everyone, :search)
+        expect(checklist_template_class).to receive_message_chain(:everyone, :paginate, :search)
         get :index, params: { search: 'sport' }
       end
     end
