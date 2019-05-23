e7df831e1687c19632b28c3a37a84dd2cb5f7aae
Artem Rakov <scoff1k@gmail.com>
2019-05-23 17:07:33 +0300

    solve es6 typos

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,9 +1,9 @@
-import flatpickr from "flatpickr"
-import "flatpickr/dist/themes/airbnb.css"
+import flatpickr from "flatpickr";
+import "flatpickr/dist/themes/airbnb.css";
 
 flatpickr(".datepicker", {
     altInput: true,
     enableTime: true,
     time_24hr: true,
     minDate: new Date()
-})
+});
