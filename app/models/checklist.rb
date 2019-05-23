66e860ba93a8342adce7fe4fe581d832338261ed
Artem Rakov <scoff1k@gmail.com>
2019-05-23 16:55:52 +0300

    added checklist and items query

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -4,8 +4,11 @@ class Checklist < ApplicationRecord
   belongs_to :user
   belongs_to :checklist_template
 
+  # find checklists that have not_done items and will be due next day
   scope :due_date_tomorrow, lambda {
-    where("due_date BETWEEN ? AND ?", Date.current.in_time_zone, Date.tomorrow.end_of_day.in_time_zone)
+    joins(:items).where(items: { state: :not_done })
+                 .where(due_date: Date.current.in_time_zone..Date.tomorrow.end_of_day.in_time_zone)
+                 .distinct
   }
 
   validates :title, presence: true, length: { maximum: 100 }
