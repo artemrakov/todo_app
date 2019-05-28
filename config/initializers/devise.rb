6e9405e1498aac22be151c95e4edee44be3a2e25
Artem Rakov <scoff1k@gmail.com>
2019-05-28 07:34:41 +0300

    done

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,7 +262,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
                   scope: 'email',
-                  info_fileds: 'email, name',
+                  info_fileds: 'email, first_name, last_name',
                   image_size: 'square',
                   secure_image_url: true
 
