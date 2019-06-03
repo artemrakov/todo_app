e50eb07434f208a4204e4175be69252d7e4f0493
Artem Rakov <scoff1k@gmail.com>
2019-06-03 18:38:29 +0300

    fix elasticsearch

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,6 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.web_console.whitelisted_ips = '172.18.0.1'
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
